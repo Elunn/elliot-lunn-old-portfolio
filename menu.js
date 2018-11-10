2f7e36f2ec0e5afbbb77fc2b98cde5f513915763
Elunn <Elunn@users.noreply.github.com>
2018-11-10 16:23:12 +0000

    site change

--- menu.js
+++ menu.js
@@ -1,8 +1,38 @@
 $(document).ready(function(){
 
     $(".burger").on("click", function(){
-        $("nav ul").toggleClass("open");
+        $("nav").toggleClass("open");
         
     });
     
 });
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
+
+
+$(document).ready(function(){
+    $('.categoryItem').click(function(){
+        var category = $(this).attr('id');
+        
+        if(category == 'all'){
+            $('.potfolioItem').addClass('hide');
+            setTimeout(function(){                $('.potfolioItem').removeClass('hide');
+            }, 300);
+        } else{
+            $('.potfolioItem').addClass('hide');
+            setTimeout(function(){
+                $('.' + category).removeClass('hide');
+            }, 300);
+        }
+    });              
+});
+
